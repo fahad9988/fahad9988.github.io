4d84099b5abe087fa0f10e7dba8ccbcb1bc21fee
fahad9988 <fahad20830@gmail.com>
2023-01-08 00:48:07 +0530

    added

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -3,28 +3,12 @@ import GitHubCalendar from "react-github-calendar";
 import ReactTooltip from "react-tooltip";
 import "./Stats.scss"
 const Calender = () => {
-  const selectLastHalfYear = (contributions) => {
-    const currentYear = new Date().getFullYear();
-    const currentMonth = new Date().getMonth();
-    const shownMonths = 8;
-
-    return contributions.filter((day) => {
-      const date = new Date(day.date);
-      const monthOfDay = date.getMonth();
-
-      return (
-        date.getFullYear() === currentYear &&
-        monthOfDay > currentMonth - shownMonths &&
-        monthOfDay <= currentMonth
-      );
-    });
-  };
+ 
   return (
     <div className="cal-div" style={{width:"90%"}} >
       <GitHubCalendar
         style={{ margin: "auto",width:"100%" }}
         username="fahad9988"
-        transformData={selectLastHalfYear}
         blockSize={27}
         fontSize={15}
         color="darkblue"
